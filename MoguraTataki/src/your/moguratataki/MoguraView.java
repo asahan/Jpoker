0a95b01cb7c2cf981c9d2a7b779f839bcdb99c99
gwoni <gwoni52@gmail.com>
2012-06-09 21:00:01 +0900

    test

--- MoguraTataki/src/your/moguratataki/MoguraView.java
+++ MoguraTataki/src/your/moguratataki/MoguraView.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.Random;
 
 //두더지 잡기 뷰
-public class MoguraView extends SurfaceView 
+public abstract class MoguraView extends SurfaceView 
 	implements SurfaceHolder.Callback,Runnable {
 		//장면 정수 (1)
 		private final static int
@@ -35,7 +35,7 @@ public class MoguraView extends SurfaceView
 			//그림읽기
 			Resources r=getResources();
 			Mogura.bmp[0]=BitmapFactory.decodeResource(r,R.drawable.mogura0);
-			Mogura.bmp[1]=BitmapFactory.decodeResource(r, R.drawable.mogua1);
+			Mogura.bmp[1]=BitmapFactory.decodeResource(r, R.drawable.mogura1);
 			Mogura.bmp[2]=BitmapFactory.decodeResource(r,R.drawable.mogura2);
 			
 			//그래픽스의 생성
